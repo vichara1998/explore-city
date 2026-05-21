d5773ccc2ba5249acef903b3ab963b42b0d82850
Vichara Tharkana <vichara0115@gmail.com>
2026-05-21 11:53:05 +0530

    Add null check before hiding SupportActionBar in DiscriptionAdisham.java

--- app/src/main/java/com/example/explorecityapp/DiscriptionAdisham.java
+++ app/src/main/java/com/example/explorecityapp/DiscriptionAdisham.java
@@ -16,7 +16,9 @@ public class DiscriptionAdisham extends AppCompatActivity {
 
         //Hide  the ActionBar
         ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
+        if (actionBar != null) {
+            actionBar.hide();
+        }
 
     }
 
