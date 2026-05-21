bc53a280b0bf3b99639539e8f0d7090dd4daa7c6
Vichara Tharkana <vichara0115@gmail.com>
2026-05-21 11:46:25 +0530

    Add null check before hiding SupportActionBar in Addismvideo.java

--- app/src/main/java/com/example/explorecityapp/Addismvideo.java
+++ app/src/main/java/com/example/explorecityapp/Addismvideo.java
@@ -34,7 +34,9 @@ public class Addismvideo extends AppCompatActivity {
                 View.SYSTEM_UI_FLAG_FULLSCREEN |
                         View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
                         View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-        getSupportActionBar().hide();
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
 
 
     }
